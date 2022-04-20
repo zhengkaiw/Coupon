917d29a2ec69fe9fc92f5fb004e66b9500a5f5f7
kevinZ <zhengkaiw851@gmail.com>
2022-04-20 15:48:10 +0800

    exception handle

---
+++ Coupon-service/coupon-common/src/main/java/com/zkw/coupon/exception/CouponException.java
@@ -0,0 +1,11 @@
+package com.zkw.coupon.exception;
+
+/**
+ * 优惠券项目通用异常定义
+ */
+public class CouponException extends Exception{
+
+    public CouponException(String message) {
+        super(message);
+    }
+}
