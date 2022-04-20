81165ef966f07d663b931927130f35cb9954c79b
kevinZ <zhengkaiw851@gmail.com>
2022-04-20 15:30:51 +0800

    initiated common

---
+++ Coupon-service/coupon-common/src/main/java/com/zkw/coupon/conf/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.zkw.coupon.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * 定制 HTTP 消息转换器
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
