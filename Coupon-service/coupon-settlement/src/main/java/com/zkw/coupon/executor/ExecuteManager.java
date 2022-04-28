d8a407090515b159af8878c0c60b89a54535142a
kevinZ <zhengkaiw851@gmail.com>
2022-04-28 09:31:11 +0800

    finished ExecuteManager

--- Coupon-service/coupon-settlement/src/main/java/com/zkw/coupon/executor/ExecuteManager.java
+++ Coupon-service/coupon-settlement/src/main/java/com/zkw/coupon/executor/ExecuteManager.java
@@ -1,9 +1,18 @@
 package com.zkw.coupon.executor;
 
+import com.zkw.coupon.constant.CouponCategory;
+import com.zkw.coupon.constant.RuleFlag;
+import com.zkw.coupon.exception.CouponException;
+import com.zkw.coupon.vo.SettlementInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 优惠券结算规则执行管理器
@@ -12,15 +21,92 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
+@SuppressWarnings("all")
 public class ExecuteManager implements BeanPostProcessor {
 
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    // 规则执行器映射
+    private static Map<RuleFlag, RuleExecutor> executorIndex = new HashMap<>(RuleFlag.values().length);
+
+    /**
+     * 优惠券结算规则计算入口
+     * 注意: 一定要保证传递进来的优惠券个数 >= 1
+     */
+    public SettlementInfo computeRule(SettlementInfo settlement) throws CouponException {
+
+        SettlementInfo result = null;
+
+        // 单类优惠券
+        if (settlement.getCouponAndTemplateInfos().size() == 1) {
+
+            // 获取优惠券的类别
+            CouponCategory category = CouponCategory.of(
+                    settlement.getCouponAndTemplateInfos().get(0).getTemplate().getCategory()
+            );
+
+            switch (category) {
+                case MANJIAN:
+                    result = executorIndex.get(RuleFlag.MANJIAN)
+                            .computeRule(settlement);
+                    break;
+                case ZHEKOU:
+                    result = executorIndex.get(RuleFlag.ZHEKOU)
+                            .computeRule(settlement);
+                    break;
+                case LIJIAN:
+                    result = executorIndex.get(RuleFlag.LIJIAN)
+                            .computeRule(settlement);
+                    break;
+                default:
+            }
+        } else {
+
+            // 多类优惠券
+            List<CouponCategory> categories = new ArrayList<>(settlement.getCouponAndTemplateInfos().size());
+
+            settlement.getCouponAndTemplateInfos().forEach(ct ->
+                    categories.add(CouponCategory.of(ct.getTemplate().getCategory())));
+            if (categories.size() != 2) {
+                throw  new CouponException("Not support for more template category");
+            } else {
+                if (categories.contains(CouponCategory.MANJIAN) && categories.contains(CouponCategory.ZHEKOU)) {
+                    result = executorIndex.get(RuleFlag.MANJIAN_ZHEKOU).computeRule(settlement);
+                } else {
+                    throw new CouponException("Not support for other template categories");
+                }
+            }
+        }
+
+        return result;
     }
 
+    /**
+     * 在 Bean 初始化之前去执行(before)
+     */
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if (!(bean instanceof RuleExecutor)) {
+            return bean;
+        }
+
+        RuleExecutor executor = (RuleExecutor) bean;
+        RuleFlag ruleFlag = executor.ruleConfig();
+
+        if (executorIndex.containsKey(ruleFlag)) {
+            throw new IllegalStateException("There is already an executor for rule flag:" + ruleFlag);
+        }
+
+        log.info("Load executor {} for rule flag {}", executor.getClass(), ruleFlag);
+        executorIndex.put(ruleFlag, executor);
+
+        return executorIndex.get(beanName);
+    }
+
+    /**
+     * 在 Bean 初始化之后去执行(before)
+     */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+        return bean;
     }
 }
